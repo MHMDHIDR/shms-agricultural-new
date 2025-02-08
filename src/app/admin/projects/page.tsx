b95c7d2fadc141a38ff50d3b17e372c713494e23
Mohammed Haydar <mr.hamood277@gmail.com>
2025-02-08 20:08:36 +0000

    projects dashboard

--- src/app/admin/projects/page.tsx
+++ src/app/admin/projects/page.tsx
@@ -1,3 +1,9 @@
 export default function ProjectsPage() {
-  return <div>ProjectsPage</div>;
+  return (
+    <section className="container mx-auto">
+      <h1 className="my-10 text-center text-xl font-bold select-none">
+        المشاريع
+      </h1>
+    </section>
+  );
 }
